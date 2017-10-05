ebaab83a5afcb88bef9690b413d25e1e88548045
Nagib Tharani <ntharani@gmail.com>
2017-10-05 14:23:35 -0700

    Output customer like statement tied to number of likes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ class App extends Component {
 
   render() {
 
-    const expectedOutput = [
-      { id: 1, text: 'No one likes this' },
-      { id: 2, text: 'Peter likes this' },
-      { id: 3, text: 'John and Mark like this' },
-      { id: 4, text: 'Paul, Lilly and Alex like this' },
-      { id: 5, text: 'Sarah, Michelle and 2 others like this' }
-    ];
+    const posts = [
+      { id: 1, likes: [] },
+      { id: 2, likes: ['Peter'] },
+      { id: 3, likes: ['John', 'Mark'] },
+      { id: 4, likes: ['Paul', 'Lilly', 'Alex'] },
+      { id: 5, likes: ['Sarah', 'Michelle', 'Alex', 'John'] }
+    ];    
+
+    // const expectedOutput = [
+    //   { id: 1, text: 'No one likes this' },
+    //   { id: 2, text: 'Peter likes this' },
+    //   { id: 3, text: 'John and Mark like this' },
+    //   { id: 4, text: 'Paul, Lilly and Alex like this' },
+    //   { id: 5, text: 'Sarah, Michelle and 2 others like this' }
+    // ];
   
     return (
       <div className="App">
@@ -25,11 +33,31 @@ class App extends Component {
         </p>
 
         <ul>
-          {expectedOutput.map((obj) => {
+          {posts.map((obj) => {
+
+            let response;
+            console.log("This iteration ", obj.likes.length)
+            switch(obj.likes.length) {
+              case 0:
+                response = "No one likes this";
+                break;
+              case 1:
+                response = `${obj.likes[0]} likes this`;
+                break;
+              case 2:
+                response = `${obj.likes[0]} and ${obj.likes[1]} like this`;
+                break;
+              case 3:
+                response = `${obj.likes[0]}, ${obj.likes[1]} and ${obj.likes[2]} like this`;
+                break;
+              default: 
+                response = `${obj.likes[0]}, ${obj.likes[1]} and ${obj.likes.length - 2} others like this`;
+            }
+
             return (
             <li className="feed-li" key={obj.id}>
               <div className="feed-index">{obj.id}</div>
-              <div className="feed-text">{obj.text}</div>
+              <div className="feed-text">{response}</div>
             </li>
             )
           })}
